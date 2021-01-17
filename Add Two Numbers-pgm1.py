5dd212c4cc25727a9b1e8e4c9bf5842b84df0a3d
Sathish0224 <60768263+Sathish0224@users.noreply.github.com>
2021-01-17 22:11:36 +0200

    Add files via upload

---
+++ Add Two Numbers-pgm1.py
@@ -0,0 +1,12 @@
+
+#Add two numbers:
+
+number1=input("Please enter your first number:  ")
+number2=input("Please enter your second number: ")
+
+#Adding two numbers:
+
+sum = int(number1) + int(number2)
+
+#Print the value:
+print("The sum of {0} and {1} is {2}".format(number1, number2, sum))

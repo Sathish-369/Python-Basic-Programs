56d9e96390202b84728ed950b820d2e33a160437
Sathish0224 <60768263+Sathish0224@users.noreply.github.com>
2021-01-18 18:43:37 +0200

    Add files via upload

---
+++ Max of Two Numbers-pgm2.py
@@ -0,0 +1,18 @@
+
+a = 5
+b = 8
+def maximum(a,b):
+
+    if a >= b:
+        return a
+    else:
+        return b
+
+print(maximum(a,b))
+
+#Using max() function
+
+a = 5
+b = 8
+maximum = max(a, b)
+print(maximum)

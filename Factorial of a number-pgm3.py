cf7c9768142e3c34ccc0a06a88440ec82139825d
Sathish0224 <60768263+Sathish0224@users.noreply.github.com>
2021-01-19 17:10:26 +0200

    Add files via upload

---
+++ Factorial of a number-pgm3.py
@@ -0,0 +1,21 @@
+def factorial(n):
+    if n<0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
+
+num = 5
+print('factorial of', num, 'is', factorial(num))
+
+# single line to find factorial:
+
+def factorial(n):
+    return 1 if (n == 1 or n == 0) else n* factorial(n -1)
+num = 9
+print('factorial of', num, 'is', factorial(num))

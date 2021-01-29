4d553a51b0b14a7f45929aea10d20a701301caf9
Sathish0224 <60768263+Sathish0224@users.noreply.github.com>
2021-01-29 22:27:37 +0200

    Add files via upload

---
+++ Find sum of array-pgm16.py
@@ -0,0 +1,18 @@
+
+
+def sum(arr):
+
+    sum = 0
+
+    for i in arr:
+        sum = sum + i
+    return (sum)
+
+arr = []
+
+arr = [12, 3, 4, 15]
+
+n = len(arr)
+ans = sum(arr)
+
+print("sum of array is", ans)

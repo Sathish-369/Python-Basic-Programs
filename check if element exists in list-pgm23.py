341741170b1ac158d7ae4808dd4296cae5ad0f77
Sathish0224 <60768263+Sathish0224@users.noreply.github.com>
2021-02-08 22:32:00 +0200

    Add files via upload

---
+++ check if element exists in list-pgm23.py
@@ -0,0 +1,13 @@
+
+
+test_list =[1,6,3,4,6,7,4]
+print("Checking if 6 exists in list (using loop): ")
+
+for i in test_list:
+    if(i == 6):
+        print("Element Exists")
+
+print("Checking if 6 exists in list (using in): ")
+
+
+

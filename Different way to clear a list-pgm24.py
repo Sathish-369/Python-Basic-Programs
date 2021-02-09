f6fd5196b1fedde8ca2f7b8e7205cbd043631dcd
Sathish0224 <60768263+Sathish0224@users.noreply.github.com>
2021-02-09 19:41:10 +0200

    Add files via upload

---
+++ Different way to clear a list-pgm24.py
@@ -0,0 +1,13 @@
+
+
+list1=[1,3,5,7]
+list2=[3,6,9,80]
+print("list1 one before deleting is:  " + str(list1))
+list1.clear()
+print("list1 after clearing using clear() : " +str(list1))
+
+print("List2 before deleting is : " + str(list2))
+
+list2 =[]
+
+print("list2 after clearing using reinitialization : "+ str(list2))

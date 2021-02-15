2a64996ce9a990602650b82db6360e9f83bc8991
Sathish-369 <60768263+Sathish-369@users.noreply.github.com>
2021-02-15 20:17:20 +0200

    Add files via upload

---
+++ Find secound largest number in the list -pgm30.py
@@ -0,0 +1,13 @@
+
+
+list1 = [11,22,33,44,55,66,77,88,99,900]
+list1.sort()
+print("largest number in the list: ", list1[-1])
+
+list1 = [11,22,33,44,55,66,77,88,99,900]
+list1.sort()
+print("2nd largest number in the list: ", list1[-2])
+
+list1 = [11,22,33,44,55,66,77,88,99,900]
+list1.sort()
+print("3rd largest number in the list: ", list1[-3])

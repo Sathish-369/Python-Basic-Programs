7634e5d924e39afef7c032e0971911ecaaf7d73c
Sathish-369 <60768263+Sathish-369@users.noreply.github.com>
2021-04-05 23:43:02 +0300

    Add files via upload

---
+++ Python function -pgm52.py
@@ -0,0 +1,10 @@
+
+def printime(str):
+    " This prints a passed string into this function"
+    mylist.append([1,2,3,4])
+    print ("Value inside the function: ", mylist)
+    return;
+mylist=[10,20,30]
+(mylist);
+printime("I am first call to user defined function!")
+printime("Again second call to the same function")

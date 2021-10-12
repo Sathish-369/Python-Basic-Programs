52a49abd780946c3abf439d7087cdd539face176
Sathish-369 <60768263+Sathish-369@users.noreply.github.com>
2021-10-12 21:43:32 +0300

    Add files via upload

---
+++ Python Maximum height whhen coins are in triangle.py
@@ -0,0 +1,24 @@
+def squareRoot(n):
+
+    x = n
+    y = 1
+
+    e = 0.000001
+    while (x - y > e):
+        x = (x + y) / 2
+        y = n / x
+
+    return x
+
+
+
+def findMaximumHeight(N):
+
+    n = 1 + 8 * N
+    maxH = (-1 + squareRoot(n)) / 2
+    return int(maxH)
+
+
+
+N = 12
+print(findMaximumHeight(N))

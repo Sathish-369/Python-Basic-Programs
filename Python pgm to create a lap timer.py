24143928eb61007e7fa4ed897ae9e37438fb652a
Sathish-369 <60768263+Sathish-369@users.noreply.github.com>
2021-10-16 20:57:30 +0300

    Add files via upload

---
+++ Python pgm to create a lap timer.py
@@ -0,0 +1,25 @@
+import time
+
+starttime = time.time()
+lasttime = startime
+lapnum = 1
+
+print("Press ENTER to count laps.\nPress CTRL+C to stop")
+try:
+    while True:
+            input()
+            laptime = round((time.time() - lasttime), 2)
+
+            totaltime = round((time.time() - starttime), 2)
+
+            print("Lap No. "+str(laptime))
+            print("Total Time: "+str(totaltime))
+            print("Lap Time: "+str(laptime))
+
+            print("+"*20)
+
+            lasttime = time.time()
+            lapnum += 1
+
+except KeyboardInterrupt:
+    print("Done")

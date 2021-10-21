e10c46bfb8aa2fea8432ac233eccb3838d978992
Sathish-369 <60768263+Sathish-369@users.noreply.github.com>
2021-10-21 22:11:12 +0300

    Add files via upload

---
+++ Python ways to remove a key from dictionary.py
@@ -0,0 +1,11 @@
+
+
+test_dict = {"Sat": 20, "Rat": 30, "Bat": 40, "Mat": 50}
+
+print("The dictionary before performing remove is :" +str (test_dict))
+
+del test_dict["Rat"]
+
+print("The dictionary after remove is : "+ str(test_dict))
+
+del test_dict['Cat']
